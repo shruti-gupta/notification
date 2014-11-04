51455240d509cddc12a196a095c5f937044d0d60
Shruti Gupta <shruti22011991@gmail.com>
2014-11-04 12:07:29 +0530

    Image added in notification

--- src/com/example/notification/MainActivity.java
+++ src/com/example/notification/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.notification;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
@@ -16,30 +19,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		NotificationCompat.Builder mBuilder =
-		        new NotificationCompat.Builder(this)
-		        .setSmallIcon(R.drawable.ic_launcher)
-		        .setContentTitle("My notification")
-		        .setContentText("Hello World!");
-		
-NotificationCompat.InboxStyle inboxStyle =
-        new NotificationCompat.InboxStyle();
+
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+				this).setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("My notification")
+				.setContentText("Hello World!");
+
+		NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
 		String[] events = new String[6];
 		// Sets a title for the Inbox in expanded layout
 		inboxStyle.setBigContentTitle("Event tracker details:");
 		// Moves events into the expanded layout
-		for (int i=0; i < events.length; i++) {
+		for (int i = 0; i < events.length; i++) {
 
-			events[i] = "abcdef"+i;
-		    inboxStyle.addLine(events[i]);
+			events[i] = "abcdef" + i;
+			inboxStyle.addLine(events[i]);
 		}
+		Bitmap icon = BitmapFactory.decodeResource(this.getResources(),
+				R.drawable.download);
+
 		// Moves the expanded layout object into the notification object.
-		mBuilder.setStyle(inboxStyle);
+		mBuilder.setStyle(
+				new NotificationCompat.BigPictureStyle().bigPicture(icon))
+				.setStyle(inboxStyle);
 		// Creates an explicit intent for an Activity in your app
 		Intent resultIntent = new Intent(this, MainActivity.class);
 
-		// The stack builder object will contain an artificial back stack for the
+		// The stack builder object will contain an artificial back stack for
+		// the
 		// started Activity.
 		// This ensures that navigating backward from the Activity leads out of
 		// your application to the Home screen.
@@ -48,19 +55,14 @@ NotificationCompat.InboxStyle inboxStyle =
 		stackBuilder.addParentStack(MainActivity.class);
 		// Adds the Intent that starts the Activity to the top of the stack
 		stackBuilder.addNextIntent(resultIntent);
-		PendingIntent resultPendingIntent =
-		        stackBuilder.getPendingIntent(
-		            0,
-		            PendingIntent.FLAG_UPDATE_CURRENT
-		        );
+		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
+				PendingIntent.FLAG_UPDATE_CURRENT);
 		mBuilder.setContentIntent(resultPendingIntent);
 		int numMessages = 0;
 		// Start of a loop that processes data and then notifies the user
-		mBuilder.setContentText("Akhil")
-		        .setNumber(++numMessages);
-		    
-		NotificationManager mNotificationManager =
-		    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mBuilder.setContentText("Akhil").setNumber(++numMessages);
+
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		// mId allows you to update the notification later on.
 		mNotificationManager.notify(1, mBuilder.build());
 		mNotificationManager.notify(1, mBuilder.build());
